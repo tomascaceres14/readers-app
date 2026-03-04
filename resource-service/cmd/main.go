f4eef55697476a4aa72efe272e5a25c1b77a6813
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2026-03-04 12:48:06 +0100

    fix: basic file searching. Must improve. Consider storing first in .html locally then process

--- resource-service/cmd/main.go
+++ resource-service/cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
+	"github.com/go-shiori/go-readability"
 	"github.com/tomascaceres14/readers-app/resource-service/internal/messaging"
 	"github.com/tomascaceres14/readers-app/resource-service/internal/scraping"
 )
@@ -22,7 +25,31 @@ func main() {
 	if err := consumer.Setup(); err != nil {
 		log.Fatal(err)
 	}
+	url := "https://www.postgresql.org/docs/current/textsearch-intro.html"
+	article, e := readability.FromURL(url, time.Minute)
+	if e != nil {
+		log.Fatal(e)
+	}
 
+	dstTxtFile, _ := os.Create("text-test.txt")
+	defer dstTxtFile.Close()
+	dstTxtFile.WriteString(article.TextContent)
+
+	dstHTMLFile, _ := os.Create("html-temp.html")
+	defer dstHTMLFile.Close()
+	dstHTMLFile.WriteString(article.Content)
+
+	fmt.Printf("URL     : %s\n", url)
+	fmt.Printf("Title   : %s\n", article.Title)
+	fmt.Printf("Author  : %s\n", article.Byline)
+	fmt.Printf("Length  : %d\n", article.Length)
+	fmt.Printf("Excerpt : %s\n", article.Excerpt)
+	fmt.Printf("SiteName: %s\n", article.SiteName)
+	fmt.Printf("Image   : %s\n", article.Image)
+	fmt.Printf("Favicon : %s\n", article.Favicon)
+	fmt.Println()
+
+	os.Exit(0)
 	consumer.Listen()
 
 	quit := make(chan os.Signal, 1)
--- resource-service/go.mod
+++ resource-service/go.mod
@@ -9,19 +9,20 @@ require (
 )
 
 require (
-	codeberg.org/readeck/go-readability/v2 v2.1.1 // indirect
 	github.com/andybalholm/cascadia v1.3.3 // indirect
 	github.com/antchfx/htmlquery v1.3.5 // indirect
 	github.com/antchfx/xmlquery v1.5.0 // indirect
 	github.com/antchfx/xpath v1.3.5 // indirect
 	github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de // indirect
 	github.com/go-shiori/dom v0.0.0-20230515143342-73569d674e1c // indirect
+	github.com/go-shiori/go-readability v0.0.0-20251205110129-5db1dc9836f0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gogs/chardet v0.0.0-20211120154057-b7413eaefb8f // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	golang.org/x/net v0.51.0 // indirect
 	golang.org/x/text v0.34.0 // indirect
